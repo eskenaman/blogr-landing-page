8e668bffa13ffcba9270003588f615d12012ef5c
eskenaman <79567781+eskenaman@users.noreply.github.com>
2021-05-06 03:19:33 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+
+if (window.innerWidth < 700) {
+  $(".product-picture").attr("src", "images/illustration-editor-mobile.svg");
+  $(".features-picture").attr("src", "images/illustration-laptop-mobile.svg");
+}
